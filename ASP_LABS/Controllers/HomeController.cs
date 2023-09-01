05c2135b87406267ccdfc85659f082418e4abb29
Ialek <i.aleksandrionok@gmail.com>
2023-09-01 18:33:28 +0300

    Add project files.

---
+++ ASP_LABS/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using ASP_LABS.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace ASP_LABS.Controllers
+{
+    public class HomeController : Controller
+    {
+       
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        public IActionResult Lab_1()
+        {
+            ViewBag.Lab = "Лабораторная работа 2";
+            List<ListDemo> list = new List<ListDemo>
+            {
+                new ListDemo() { Id = 1, Name = "name1" },
+                new ListDemo() { Id = 2, Name = "name2" },
+                new ListDemo() { Id = 3, Name = "name3" }
+            };
+            ViewBag.List = new SelectList(list, "Id", "Name");
+
+
+            return View();
+        }
+
+
+    }
+}
---
+++ ASP_LABS/ViewComponents/Cart.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ASP_LABS.ViewComponents
+{
+    [ViewComponent]
+    public class Cart
+    {
+        public HtmlString Invoke()
+        {
+            double price = 0;
+            int amount = 0;
+            return new HtmlString($"{price} руб <i class=\"fa-solid fa-cart-shopping\"></i>({amount})");
+
+        }
+    }
+}
