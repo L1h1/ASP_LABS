43a7c5f895876c02b3e71a6e6cfe85df375d2510
Ialek <i.aleksandrionok@gmail.com>
2023-09-04 17:44:47 +0300

    L4 api generated

---
+++ ASP_LABS.API/Controllers/GenreController.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using ASP_LABS.API.Data;
+using ASP_LABS.Domain.Entities;
+
+namespace ASP_LABS.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenreController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GenreController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Genre
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Genre>>> GetGenreSet()
+        {
+          if (_context.GenreSet == null)
+          {
+              return NotFound();
+          }
+            return await _context.GenreSet.ToListAsync();
+        }
+
+        // GET: api/Genre/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Genre>> GetGenre(int id)
+        {
+          if (_context.GenreSet == null)
+          {
+              return NotFound();
+          }
+            var genre = await _context.GenreSet.FindAsync(id);
+
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            return genre;
+        }
+
+        // PUT: api/Genre/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutGenre(int id, Genre genre)
+        {
+            if (id != genre.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(genre).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!GenreExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Genre
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Genre>> PostGenre(Genre genre)
+        {
+          if (_context.GenreSet == null)
+          {
+              return Problem("Entity set 'AppDbContext.GenreSet'  is null.");
+          }
+            _context.GenreSet.Add(genre);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetGenre", new { id = genre.Id }, genre);
+        }
+
+        // DELETE: api/Genre/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteGenre(int id)
+        {
+            if (_context.GenreSet == null)
+            {
+                return NotFound();
+            }
+            var genre = await _context.GenreSet.FindAsync(id);
+            if (genre == null)
+            {
+                return NotFound();
+            }
+
+            _context.GenreSet.Remove(genre);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool GenreExists(int id)
+        {
+            return (_context.GenreSet?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
--- ASP_LABS.API/Data/DbInitializer.cs
+++ ASP_LABS.API/Data/DbInitializer.cs
@@ -11,6 +11,14 @@ namespace ASP_LABS.API.Data
 			System.Diagnostics.Debug.WriteLine("---------------Entered SeedData");
 			using var scope = app.Services.CreateScope();
 			var context = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+			context.Database.EnsureDeleted();
+			context.Database.EnsureCreated();
+			string? imagesUrl = app.Configuration.GetSection("ImagesUrl").Get<string>();
+
+			if (string.IsNullOrEmpty(imagesUrl))
+			{
+				throw new Exception("Missing images url");
+			}
 
 			//await context.Database.MigrateAsync();
 
@@ -28,13 +36,13 @@ namespace ASP_LABS.API.Data
 			await context.SaveChangesAsync();
 			System.Diagnostics.Debug.WriteLine($"---------------number of genres registered:{context.GenreSet.Count()}");
 
-			await books.AddAsync(new Book() { Title = "FirstTitle", Genre = genres.First(g=>g.Name=="Thriller"), Price = 40.99 });
-			await books.AddAsync(new Book() { Title = "SecondTitle", Genre = genres.First(g => g.Name == "Horror"), Price = 40.99 });
-			await books.AddAsync(new Book() { Title = "ThirdTitle", Genre = genres.First(g => g.Name == "Science Fiction"), Price = 40.99, Description = "Knife of Dunwall" });
-			await books.AddAsync(new Book() { Title = "FourthTitle", Genre = genres.First(g => g.Name == "Thriller"), Price = 14.99 });
-			await books.AddAsync(new Book() { Title = "FifthTitle", Genre = genres.First(g => g.Name == "Science Fiction"), Price = 74.99 });
-			await books.AddAsync(new Book() { Title = "SixthTitle", Genre = genres.First(g => g.Name == "Fantasy"), Price = 24.99 });
-			await books.AddAsync(new Book() { Title = "SeventhTitle", Genre = genres.First(g => g.Name == "Fantasy"), Price = 18.99 });
+			await books.AddAsync(new Book() { Title = "FirstTitle", Genre = genres.First(g=>g.Name=="Thriller"), Price = 40.99,ImagePath=$"temp: {imagesUrl}" });
+			await books.AddAsync(new Book() { Title = "SecondTitle", Genre = genres.First(g => g.Name == "Horror"), Price = 40.99, ImagePath = $"temp: {imagesUrl}"  });
+			await books.AddAsync(new Book() { Title = "ThirdTitle", Genre = genres.First(g => g.Name == "Science Fiction"), Price = 40.99, Description = "Knife of Dunwall", ImagePath = $"temp: {imagesUrl}" });
+			await books.AddAsync(new Book() { Title = "FourthTitle", Genre = genres.First(g => g.Name == "Thriller"), Price = 14.99, ImagePath = $"temp: {imagesUrl}" });
+			await books.AddAsync(new Book() { Title = "FifthTitle", Genre = genres.First(g => g.Name == "Science Fiction"), Price = 74.99, ImagePath = $"temp: {imagesUrl}" });
+			await books.AddAsync(new Book() { Title = "SixthTitle", Genre = genres.First(g => g.Name == "Fantasy"), Price = 24.99, ImagePath = $"temp: {imagesUrl}" });
+			await books.AddAsync(new Book() { Title = "SeventhTitle", Genre = genres.First(g => g.Name == "Fantasy"), Price = 18.99, ImagePath = $"temp: {imagesUrl}" });
 			System.Diagnostics.Debug.WriteLine($"---------------number of books registered:{context.BookSet.Count()}");
 
 
