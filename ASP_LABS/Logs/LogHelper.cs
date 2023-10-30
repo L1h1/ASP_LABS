89f25e300227cc58e00db4be1499c3ddaa464c12
Ialek <i.aleksandrionok@gmail.com>
2023-10-30 17:29:08 +0300

    l11 fix

---
+++ ASP_LABS/Logs/LogHelper.cs
@@ -0,0 +1,29 @@
+﻿using Serilog.Events;
+
+namespace ASP_LABS.Logs
+{
+    public static class LogHelper
+    {
+        private static bool IsOK(HttpContext ctx)
+        {
+            var code = ctx.Response.StatusCode;
+            if (code < 300)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public static LogEventLevel ExcludeHealthChecks(HttpContext ctx, double _, Exception ex) =>
+        ex != null
+            ? LogEventLevel.Error
+            : ctx.Response.StatusCode > 499
+                ? LogEventLevel.Error
+                : IsOK(ctx)
+                    ? LogEventLevel.Verbose
+                    : LogEventLevel.Information;
+
+    }
+
+}
